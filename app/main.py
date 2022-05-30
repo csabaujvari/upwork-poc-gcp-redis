df5594183c8dafbd325d9a6c3fb05a93bfb26b8e
Csaba Ujvári <csaba.ujvari@commitsmart.com>
2022-05-30 13:58:26 +0200

    feat(app): create python app to communicate with redis

---
+++ app/main.py
@@ -0,0 +1,33 @@
+import logging
+import os
+
+from flask import Flask
+import redis
+
+app = Flask(__name__)
+
+redis_host = os.environ.get('REDISHOST', 'localhost')
+redis_port = int(os.environ.get('REDISPORT', 6379))
+redis_client = redis.StrictRedis(host=redis_host, port=redis_port)
+
+
+@app.route('/')
+def index():
+    value = redis_client.incr('counter', 1)
+    return 'Visitor number: {}'.format(value)
+
+
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+
+
+if __name__ == '__main__':
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine and Cloud Run.
+    # See entrypoint in app.yaml or Dockerfile.
+    app.run(host='127.0.0.1', port=8080, debug=True)
